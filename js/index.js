c015feb958b8f5164c6cad7486fcd615938d6255
gasparliboreiro <liboreirogaspar@gmail.com>
2025-10-25 13:37:14 -0300

    nota para el siguiente programador

--- js/index.js
+++ js/index.js
@@ -2,6 +2,32 @@ console.log("index js cargado");
 
 
 
+/*
+mensaje para el siguiente programador que tenga que interactuar con esto :)
+
+este codigo fue hecho con apuro y sin experiencia, esta fue mi segunda pagina que complete en mi vida, asi que me di cuenta
+de cosas que haria distinto a mitad de camino
+
+en general la filosofia que mantiene esta pagina de pie es:
+-  en un basurero, tenes permitido todo lo que este al alcance
+
+si nesecitas una funcion, hacela en cualquier lado, si nesecitas la informacion de la db, es global, si nesecitas una nueva
+clase de css, ponela al final del styles.css, tu codigo es feo y repetitivo? me da igual
+
+no interactues con nada a menos que sea totalmente necesario, cada archivo es un mundo, esto no usa ninguna api a parte de 
+la de firebase, asi q si sabes js estas listo para descifrar esto
+
+suerte ojala no sea imposible programar aca :3 materias correlativas: reverse engineering en software
+*/
+
+
+/*
+si llega a explotar algo o nesecitas un dato que no podes averiguar de otra forma, pedile al dueño del corralon que queres hablar con Gaspar
+
+lo mas raro de todo el el tema de la autenticacion con firebase, quiza no lo pareza en primer instancia
+*/
+
+
 
 // <!1> tener en la db 2 "carpetas" data y test-data, cuando el admin quiere añadir un producto o hacer cosas asi, lo hace en test-dat¿a y despues "commitea" y se transfiere todo a data
 
